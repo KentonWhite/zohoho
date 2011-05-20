a186b33ca90cdbb5b4818dea4bd0e1d2b6460423
KentonWhite <jkentonwhite@gmail.com>
2011-05-20 11:45:53 -0400

    Implements Connection@call to make simple calls to the zoho service

--- lib/zohoho/connection.rb
+++ lib/zohoho/connection.rb
@@ -1,21 +1,66 @@
 module Zohoho 
-  require 'httparty' 
+  require 'httparty'
+  require 'json'
   
   class Connection
     include HTTParty
     
-    def initialize(username, password)
-      @username, @password = username, password
+    def initialize(service_name, username, password, apikey, zoho_uri = 'https://crm.zoho.com/crm/private/json')
+      @service_name, @username, @password, @apikey, @zoho_uri = service_name, username, password, apikey, zoho_uri
     end 
     
-    def ticket_url(service_name)
-      "https://accounts.zoho.com/login?servicename=Zoho#{service_name}&FROM_AGENT=true&LOGIN_ID=#{@username}&PASSWORD=#{@password}"
+    def ticket_url
+      "https://accounts.zoho.com/login?servicename=Zoho#{@service_name}&FROM_AGENT=true&LOGIN_ID=#{@username}&PASSWORD=#{@password}"
     end 
     
-    def ticket(service_name)
-      url = ticket_url(service_name) 
+    def api_key
+      @api_key
+    end 
+    
+    def zoho_uri
+      @zoho_uri
+    end
+    
+    def ticket
+      return @ticket if @ticket
+      url = ticket_url 
       ticket_info = self.class.post(url).parsed_response 
       ticket_info.match(/\sTICKET=(.*)\s/)[1]
+    end 
+    
+    def call(entry, api_method, query = {}, http_method = :get)
+      login = {
+        :apikey => api_key,
+        :ticket => ticket
+      }    
+      query.merge!(login)
+
+     url = [zoho_uri, entry, api_method].join('/')
+     case http_method
+      when :get       
+        raw = JSON.parse(self.class.get(url, :query => query).parsed_response)
+        parse_raw(raw, entry)    
+      when :post
+        JSON.parse(self.class.post(url, :body => query).parsed_response)
+      else
+        raise "#{http_method} is not a recognized http method"
+      end      
+    end 
+    
+    private
+    
+    def parse_raw(raw, entry)
+      return [] if raw['response']['result'].nil?
+      rows = raw['response']['result'][entry]['row'] 
+      rows = [rows] unless rows.class == Array
+      rows.map {|i|
+        raw_to_hash i['FL']
+      }
+    end
+
+    def raw_to_hash(raw)
+      raw.map! {|r| [r['val'], r['content']]}
+      Hash[raw]
     end
     
   end 
--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -5,18 +5,27 @@ describe "Zohoho::Connection" do
   before :each do
     @username = 'kentonwhite'
     @password = 'mopa3lwb'
-    @conn = Zohoho::Connection.new(@username, @password) 
-    vcr_config 'crm'    
+    @apikey = 'L-PvsrDNn9EIW2phA3vzp9YuL5REECogkQaMGWeIdlI$'
+    @conn = Zohoho::Connection.new('CRM', @username, @password, @apikey) 
+    vcr_config 'connection'    
   end
   it "should create the proper ticket url" do
     ticket_url = "https://accounts.zoho.com/login?servicename=ZohoCRM&FROM_AGENT=true&LOGIN_ID=#{@username}&PASSWORD=#{@password}"
-    @conn.ticket_url('CRM').should == ticket_url
+    @conn.ticket_url.should == ticket_url
   end
   
   it "should get a new ticket" do
     VCR.use_cassette('ticket', :record => :new_episodes) do
-      @ticket = @conn.ticket('CRM')
+      @ticket = @conn.ticket
     end
     @ticket.should == 'c1eea66055af5d42b8b1ab0d1171c9c4'
+  end 
+  
+  it "should make a simple call" do
+    VCR.use_cassette('call', :record => :new_episodes) do
+      @result = @conn.call('Contacts', 'getRecords')
+    end
+    @result.size.should == 5
   end
+  
 end
